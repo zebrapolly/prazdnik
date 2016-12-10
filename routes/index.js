d0c888c63717fdbc8f04f7f2c8f51ee84ce2a46d
Smirnov Mikhail <zebrapolly@gmail.com>
2016-12-10 17:18:09 +0300

    setting port

--- routes/index.js
+++ routes/index.js
@@ -16,8 +16,8 @@ module.exports = function(app, transporter) {
         res.send('Mailsend');
     });
 
-    app.listen(3000, function () {
-        console.log('Example app listening on port 3000!');
+    app.listen(process.env.PORT || 3000, function(){
+        console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
     });
 
 }
